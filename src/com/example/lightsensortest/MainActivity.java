4e957b1728d514a1210169e16c749a43301573b3
Administrator <Administrator@2013-20150818JH>
2016-04-19 08:18:33 +0800

    8.18

---
+++ src/com/example/lightsensortest/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.lightsensortest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private TextView light_text;
+	private SensorManager manager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		light_text = (TextView) findViewById(R.id.light_text);
+		manager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		Sensor sensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+		manager.registerListener(listener, sensor,
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	private SensorEventListener listener = new SensorEventListener() {
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			// TODO Auto-generated method stub
+			// values数组中第一个下标的值就是当前光照强度
+			float value = event.values[0];
+			light_text.setText("当前光照强度为：" + value + "lx");
+		}
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			// TODO Auto-generated method stub
+
+		}
+	};
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		if (manager != null) {
+			manager.unregisterListener(listener);
+		}
+	}
+
+}
